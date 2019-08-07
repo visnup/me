b3bb72f21e0d3c732a3b080e6d2702d822c8cb4c
Visnu Pitiyanuvath <visnup@gmail.com>
2019-08-07 15:58:33 -0700

    videos convert better than Nelson

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ import Head from "../components/head";
 export default () => (
   <div>
     <Head title="Home" />
-    Hi Nelson
-    <style jsx>{`
-      div {
-        text-align: center;
-      }
-    `}</style>
+    <iframe
+      width="800"
+      height="450"
+      src="https://www.youtube.com/embed/videoseries?list=PLdRZWLkK99Q9TMyYJ0ZlpUo95AIrx0qBg"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
   </div>
 );
